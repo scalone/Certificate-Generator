7fc2cc4fe92d0e2a4c8925400bd81ca4954463fb
Thiago Scalone <thiago@scalone.com.br>
2010-11-27 23:16:22 -0200

    rails structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testee::Application.config.secret_token = 'a9c6678407ce4d9a67775656f42e79c4c56bf80b855aca1a38184256bd88fb0d5532a412a4a844fbeb6bdbfeaf539d6b3151a75fa45cbb6cff1729dc40d55a61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testee::Application.config.session_store :cookie_store, :key => '_testee_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Testee::Application.config.session_store :active_record_store
